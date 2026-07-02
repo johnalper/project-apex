537ccf783b2baaa0a2f50c7c5b233707ff2f2607
John Alper <214384760+johnalper@users.noreply.github.com>
2026-07-02 16:11:44 -0400

    add page schema

---
+++ apps/studio/schemaTypes/page.ts
@@ -0,0 +1,26 @@
+import { defineType, defineField } from 'sanity';
+
+export const pageType = defineType({
+    name: 'page',
+    title: 'Fragment Page',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            type: 'string',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: { source: 'title' },
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'modules',
+            title: 'Content Fragments',
+            type: 'array',
+            of: [],
+        }),
+    ],
+});
