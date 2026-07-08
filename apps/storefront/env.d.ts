38d3a484189e6a817760c3db04392d19545774b4
John Alper <214384760+johnalper@users.noreply.github.com>
2026-07-08 12:21:31 -0400

    Register env variable types

--- apps/storefront/env.d.ts
+++ apps/storefront/env.d.ts
@@ -5,3 +5,12 @@
 
 // Enhance TypeScript's built-in typings.
 import '@total-typescript/ts-reset';
+
+// Sanity Variables (keeping it simple)
+interface Env {
+    SESSION_SECRET: string;
+    PUBLIC_STORE_DOMAIN: string;
+    PUBLIC_SANITY_PROJECT_ID: string;
+    PUBLIC_SANITY_DATASET: string;
+    PUBLIC_SANITY_API_VERSION: string;
+}
